5620f72ae609defc9a335d228b65ea181ac4735c
JavierMoreno30 <jaamosi2006@gmail.com>
2026-04-15 17:06:12 -0600

    Avances lunes

--- src/main/java/model/Search.java
+++ src/main/java/model/Search.java
@@ -61,7 +61,8 @@ public class Search {
         steps.add("No encontrado");
         return -1;
     }
-    public static class MinMax{
+
+    public static class MinMax {
         public int min;
         public int max;
 
@@ -70,41 +71,46 @@ public class Search {
             this.max = max;
         }
 
-        public int getMin(){return min;}
-        public int getMax(){return max;}
+        public int getMin() {
+            return min;
+        }
+
+        public int getMax() {
+            return max;
+        }
 
     }
     public static MinMax findMinMax(int[] arr, int low, int high) {
-        //Caso bae: solo hay un elemento
-        if (low == high) {
+        //Case base: solo hay un elemento
+        if(low==high){
             steps.add("Caso base 1 elemento: min=="+arr[low]+" max=="+arr[high]);
-            return new MinMax(arr[low], arr[high]);
+            return new MinMax(arr[low], arr[low]);
         }
         //Caso base: dos elementos
-        if (high == low + 1) {
-            steps.add("Caso base 2 elementos: min=="+Math.min(arr[low], arr[high])+" max=="+Math.max(arr[low], arr[high]));
+        if(high == low + 1){
+            steps.add("Caso base 2 elementos: min=="+Math.min(arr[low], arr[high])+
+                    ", max=="+Math.max(arr[low], arr[high]));
             return new MinMax(Math.min(arr[low], arr[high]), Math.max(arr[low], arr[high]));
         }
 
         //En otro caso, debemos dividir el arreglo en dos mitades
-        int mid = (low + high) / 2;
-        steps.add("Rango [" + low + "," + high + "] -->mid=" + mid);
-        steps.add("left result: Rango [" + low + "," + mid + "]");
-
-        MinMax leftResult = findMinMax(arr, low, mid);
-        steps.add(" Right result: Rango [" + mid+1 + "," + high + "]");
-
-
-        MinMax rightResult = findMinMax(arr, mid + 1, high);
+        int mid = (low+high)/2;
+        steps.add("Rango ["+low+","+high+"] -->mid="+mid+", arr[mid]=="+arr[mid]);
+        steps.add("leftResult = findMinMax(arr,"+low+","+mid+") -->low=="+low
+                +", arr[low]=="+arr[low]+", high=="+mid+", arr[mid]=="+arr[mid]);
+        MinMax leftResult = findMinMax(arr, low, mid); //la mitad a la izq
+        steps.add("rightResult = findMinMax(arr,"+mid+1+","+high+") -->low=="+mid+1
+                +", arr[mid+1]=="+arr[mid+1]+", high=="+high+", arr[high]=="+arr[high]);
+        MinMax rightResult = findMinMax(arr, mid+1, high); //la mitad a la der
 
         //Podemos combinar los resultados
         int min = Math.min(leftResult.getMin(), rightResult.getMin());
-        steps.add("Valor de la variable min:"+min);
-
+        steps.add("valor de la variable min =="+min);
         int max = Math.max(leftResult.getMax(), rightResult.getMax());
-        steps.add("Valor de la variable max:"+max);
+        steps.add("valor de la variable max =="+max);
 
         return new MinMax(min, max);
     }
+
 
 }
--- src/test/java/model/SearchTest.java
+++ src/test/java/model/SearchTest.java
@@ -3,24 +3,56 @@ package model;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SearchTest {
     @Test
-
     void minMaxTest() {
-        int[] arr = new Random().ints(100,1,100).toArray();
+        int[] arr = new Random().ints(9, 1, 100).toArray();
         System.out.println("\n" + Arrays.toString(arr));
-        Search.MinMax minMax = Search.findMinMax(arr, 0, arr.length-1);
+        Search.steps.clear();//limpiamos el arrayList
+        Search.MinMax minMax = Search.findMinMax(arr, 0, arr.length - 1);
+        System.out.println("Pasos realizados por el algoritmo recursivo");
         System.out.println("Array min item: " + minMax.getMin());
 
         //Mostramos el contenido del arrayList
-        for(String s : Search.steps){
+        for (String s : Search.steps) {
             System.out.println(s);
         }
     }
 
-}//a
+    @Test
+    void binarySearchTest() {
+        int[] arr = util.Utility.generatedSorted(100, 100);
+        System.out.println("\n" + Arrays.toString(arr));
+
+        for (int i = 0; i < 20; i++) {
+            int value = new Random().nextInt(100); //valor a buscar
+            System.out.println(binSearchResult("java.util.Arrays", arr, value)); //Arrays.binarySearch(arr, value);
+            System.out.println(binSearchResult("java.util.Collections", arr, value)); //Collections.binarySearch(list, value);
+            System.out.println(binSearchResult("recursive model.Search", arr, value)); //Search.binarySearch(arr, value, 0, arr.length - 1);
+            System.out.println(binSearchResult("iterative model.Search", arr, value)); //Search.binarySearch(arr, value); 
+            System.out.println("_".repeat(50));
+        }
+    }
+
+    private String binSearchResult(String searchType, int[] arr, int value) {
+        int index = switch (searchType) {
+            case "java.util.Arrays" -> Arrays.binarySearch(arr, value);
+            case "java.util.Collections" -> {
+                List<Integer> list = Arrays.stream(arr).boxed().toList();
+                yield Collections.binarySearch(list, value);
+            }
+            case "recursive model.Search" -> Search.binarySearch(arr, value, 0, arr.length - 1);
+            case "iterative model.Search" -> Search.binarySearchIterative(arr, value);
+            default -> 0;
+        };
+        return index < 0 ? searchType+"value [" + value + "] index==" + index
+                :searchType+ "value [" + value + "] not found";
+    }
+}
 
