3e5b32ddc9e62129e3c5955e2eacf7e9278579af
JavierMoreno30 <jaamosi2006@gmail.com>
2026-04-15 18:49:01 -0600

    Hello

--- src/main/java/ucr/lab/pg03a/HelloApplication.java
+++ src/main/java/ucr/lab/pg03a/HelloApplication.java
@@ -6,13 +6,27 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        URL fxmlLocation = HelloApplication.class.getResource("/main.fxml");
+
+        if (fxmlLocation == null) {
+            throw new RuntimeException("No se encontró main.fxml");
+        }
+
+        FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 760);
+
+        URL cssLocation = HelloApplication.class.getResource("/styles.css");
+        if (cssLocation != null) {
+            scene.getStylesheets().add(cssLocation.toExternalForm());
+        }
+
+        stage.setTitle("PG-03");
         stage.setScene(scene);
         stage.show();
     }
