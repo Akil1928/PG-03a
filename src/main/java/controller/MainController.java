d3d7858c8472836fc11542125f3e1fe9d6dbf5dc
JavierMoreno30 <jaamosi2006@gmail.com>
2026-04-15 20:00:12 -0600

    actualizado

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -7,8 +7,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
+import model.Painter;
+import model.Probabilistic;
 
 
+import java.math.BigInteger;
 import java.net.URL;
 import java.util.*;
 
@@ -16,8 +19,6 @@ public class MainController implements Initializable {
 
     @FXML
     private TabPane mainTabPane;
-
-
     @FXML
     private Button btnGenerar;
     @FXML
@@ -27,7 +28,7 @@ public class MainController implements Initializable {
     @FXML
     private ListView listRegistroOperaciones;
     @FXML
-    private Spinner spinnerRounds;
+    private Spinner <BigInteger> spinnerRounds;
     @FXML
     private Button btnLimpiarTodo;
     @FXML
@@ -43,8 +44,56 @@ public class MainController implements Initializable {
     @FXML
     private Label lblBigInteger;
 
+
+    //TAB 1 - Atributos Internos
+    BigInteger min = new BigInteger("0");
+    BigInteger max = new BigInteger("1000000000000000000000000000");
+    BigInteger initial = new BigInteger("10000000000000000000000");
+    BigInteger step = new BigInteger("1");
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        setupMillerRabin();
 
     }
+
+    private void setupMillerRabin() {
+        //spinnerRounds.setValueFactory(new BigIntegerSpinnerValueFactory(min, max, initial, step));
+
+        spinnerRounds.valueProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue != null) {
+                lblBigInteger.setText("Número de rondas: " + newValue.toString());
+            }
+        });
+
+        btnMillerRabin.setOnAction(actionEvent -> runMillerRabin());
+        btnMillerRabin.setOnAction(actionEvent -> reset(1));
+        btnLimpiarCampo.setOnAction(actionEvent -> {lblBigInteger.setText("");});
+
+    }
+
+    private void runMillerRabin() {
+        Probabilistic probabilistic = new Probabilistic();
+        String result = probabilistic.millerRabin(lblBigInteger.getText());
+        ObservableList<String> items = FXCollections.observableArrayList();
+        if(result.contains("is probably prime")) {
+            //tenemos que agregarlo al table view
+            items.add(result+" ✔");
+        }else{
+            items.add(result+" ❌");
+        }
+        listRegistroOperaciones.setItems(items);
+    }
+
+
+private void reset(int index) {
+        switch (index) {
+            case 1: //Tab 1
+                Painter.paintEmpty(canvasMillerRabin,"Presione Miller Rabin para comenzar");
+                listRegistroOperaciones.getItems().clear();
+                break;
+            case 2:
+                break;
+        }
+}
 }
